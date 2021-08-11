c21a61d3b0de63f94c09224d096e7d1a680e1b2c
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-08-11 13:46:24 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,57 @@
 
+import React from 'react' 
+
+export class Conteiner extends React.Component {
+  state = {
+    count: 0
+  }
+
+
+increment = () => {
+  let currentYear = this.state.count;
+  currentYear++;
+  this.setState({
+    count: currentYear
+  })
+}
+
+decrement = () => {
+  let current = this.state.count;
+  current--;
+  this.setState({
+    count: current
+  })
+}
+
+reset = () => {
+  let reset = this.state.count;
+  reset = 0
+  this.setState({
+    count: reset
+  })
+}
+
+
+
+render() {
+  return(
+    <>
+  <div>
+    <button onClick={this.increment}>button</button>
+    
+  </div>
+  <div>
+  <button onClick={this.decrement}>button</button>
+</div>
+<div>
+<button onClick={this.reset}>button</button>
+</div>
+<div>
+  {this.state.count}
+</div>
+</>
+  )
+}
+
+
+}
